38fb9685210ea01ccbcb6594f86dfd5f192109ed
Cory <coryfay97@gmail.com>
2020-11-28 19:13:55 -0800

    working with local storage

--- logic.js
+++ logic.js
@@ -1,9 +1,40 @@
 $(document).ready(function () {
-    var obj = {};
-    var obj5Day = {};
+    var obj = [];
+    var obj5Day = [];
+    //var lsObj = JSON.parse(localStorage.getItem('currentDayWeather'));
+    // console.log(localStorage.getItem('currentDayWeather'));
+    // console.log(localStorage.getItem('fiveDayForecast'));
+    // var today = new Date();
+    // var dd = today.getDate();
+    // var mm = today.getMonth() + 1;
+    // var yyyy = today.getFullYear();
+    // if (dd < 10) {
+    //     dd = '0' + dd;
+    // }
+    // if (mm < 10) {
+    //     mm = '0' + mm;
+    // }
+    // today = mm + '/' + dd + '/' + yyyy;
+    // var cityDate = $('#fillList').children('p')[0];
+    //         var date = today;
+    //         var temp = $('#fillList').children('p')[1];;
+    //         var humidity = $('#fillList').children('p')[2];;
+    //         var wind = $('#fillList').children('p')[3];;
+    //         var img = $('#image');
 
-    $("#submit").on("click", function (event) {
+    //         // obj = current;
+    //         cityDate.innerText = lsObj.name + ' (' + date + ')';
+    //         temp.innerText = 'Temperature: ' + lsObj.main.temp + ' degrees';
+    //         humidity.innerText = 'Humidity: ' + lsObj.main.humidity + '%';
+    //         wind.innerText = 'Wind Speed: ' + lsObj.wind.speed + ' mph';
+    //         img.attr('src', 'http://openweathermap.org/img/wn/' + lsObj.weather[0].icon + '@2x.png')
+    //         //localStorage.setItem('currentDayWeather', JSON.stringify(obj));
+        
+
+
+    $("button").on("click", function (event) {
         event.preventDefault();
+        $('.forecast').children('div').empty();
         var city = $('#cityInput').val();
         var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762';
         var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762'
@@ -27,19 +58,22 @@ $(document).ready(function () {
             url: queryURL,
             method: 'GET'
         }).then(function (current) {
+            obj = current;
             var cityDate = $('#fillList').children('p')[0];
             var date = today;
             var temp = $('#fillList').children('p')[1];;
             var humidity = $('#fillList').children('p')[2];;
             var wind = $('#fillList').children('p')[3];;
             var img = $('#image');
+            var newH3 = $('<h3>' +obj.name+ '</h3>');
 
-            obj = current;
+            $('#emptyBtnDiv').append(newH3);
             cityDate.innerText = obj.name + ' (' + date + ')';
             temp.innerText = 'Temperature: ' + obj.main.temp + ' degrees';
             humidity.innerText = 'Humidity: ' + obj.main.humidity + '%';
             wind.innerText = 'Wind Speed: ' + obj.wind.speed + ' mph';
             img.attr('src', 'http://openweathermap.org/img/wn/' + obj.weather[0].icon + '@2x.png')
+            localStorage.setItem('currentDayWeather', JSON.stringify(obj));
         });
 
         // AJAX call for 5 day forecast
@@ -65,14 +99,13 @@ $(document).ready(function () {
                 var dateString = mm2 + "/" + dd2 + "/" + yyyy2;
                 // Fill the empty Divs with data from API 
                 obj5Day = forecast;
-                console.log(obj5Day);
                 selector.append('<h2>' + dateString + '</h2>');
                 imgForecast.attr('src', 'http://openweathermap.org/img/wn/' + obj5Day.list[3 + (i * 8)].weather[0].icon + '@2x.png')
                 selector.append(imgForecast);
                 selector.append('<p>Temp: ' + obj5Day.list[3 + (i * 8)].main.temp_max + '</p>');
                 selector.append('<p>Humidity: ' + obj5Day.list[3 + (i * 8)].main.humidity + '%</p>');
+                localStorage.setItem('fiveDayForecast', JSON.stringify(obj5Day));
             };
         });
-
     });
 });
