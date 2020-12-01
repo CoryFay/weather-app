66d74cc1bd3ac324bc7528227b85d382e48e7447
Cory <coryfay97@gmail.com>
2020-12-01 13:54:46 -0800

    further work with TA/ cleaning up code

--- logic.js
+++ logic.js
@@ -7,28 +7,49 @@ $(document).ready(function () {
         $('#forecast').children('div').empty();
         $('#forecast').removeClass('hide');
         var city = $('#cityInput').val();
-        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762';
-        var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762';
+        currentDay(city);
+        //var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762';
+        //var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762';
         // APIkey = ed95b778408ac6d4e0bb101e786ed762
 
         // Formats the Current Date
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth() + 1;
-        var yyyy = today.getFullYear();
-        if (dd < 10) {
-            dd = '0' + dd;
-        }
-        if (mm < 10) {
-            mm = '0' + mm;
-        }
-        today = mm + '/' + dd + '/' + yyyy;
+        // var today = new Date();
+        // var dd = today.getDate();
+        // var mm = today.getMonth() + 1;
+        // var yyyy = today.getFullYear();
+        // if (dd < 10) {
+        //     dd = '0' + dd;
+        // }
+        // if (mm < 10) {
+        //     mm = '0' + mm;
+        // }
+        // today = mm + '/' + dd + '/' + yyyy;
 
         // AJAX call for current weather
+
+        // AJAX call for 5 day forecast
+        
+    });
+
+    function currentDay(city) {
+        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762';
+        var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&APPID=ed95b778408ac6d4e0bb101e786ed762';
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function (current) {
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1;
+            var yyyy = today.getFullYear();
+            if (dd < 10) {
+                dd = '0' + dd;
+            }
+            if (mm < 10) {
+                mm = '0' + mm;
+            }
+            today = mm + '/' + dd + '/' + yyyy;
+
             obj = current;
             var cityDate = $('#fillList').children('p')[0];
             var date = today;
@@ -46,8 +67,6 @@ $(document).ready(function () {
             img.attr('src', 'http://openweathermap.org/img/wn/' + obj.weather[0].icon + '@2x.png')
             localStorage.setItem('currentDayWeather', JSON.stringify(obj));
         });
-
-        // AJAX call for 5 day forecast
         $.ajax({
             url: fiveDayURL,
             method: 'GET'
@@ -76,7 +95,8 @@ $(document).ready(function () {
                 selector.append('<p>Temp: ' + obj5Day.list[3 + (i * 8)].main.temp_max + '</p>');
                 selector.append('<p>Humidity: ' + obj5Day.list[3 + (i * 8)].main.humidity + '%</p>');
                 localStorage.setItem('fiveDayForecast', JSON.stringify(obj5Day));
-            };
+          };
         });
-    });
+    };
+
 });
